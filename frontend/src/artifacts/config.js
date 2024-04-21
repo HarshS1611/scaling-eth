eaba8c7a4e364f6051c7baaa1c23ce55cb689e6a
Harsh Singh <harshsingh16d@gmail.com>
2024-04-21 21:21:20 +0530

    updated filcoin address

--- frontend/src/artifacts/config.js
+++ frontend/src/artifacts/config.js
@@ -3,5 +3,5 @@ export const Ccontract_add = "0x5ED7019fCA0E2eD33167c2792c07d1751a326873";
 export const Acontract_add="0x82CD8B43D5CEad599884c721dA8FF4F399f7d7B0";
 export const Mcontract_add="0xc1D66207f079Ea69035C8C436BA5754465feB15D";
 export const Gcontract_add="0xc1D66207f079Ea69035C8C436BA5754465feB15D";
-export const Fcontract_add="0xc1D66207f079Ea69035C8C436BA5754465feB15D";
+export const Fcontract_add="0x82CD8B43D5CEad599884c721dA8FF4F399f7d7B0";
 export const Avcontract_add="0x82CD8B43D5CEad599884c721dA8FF4F399f7d7B0";
